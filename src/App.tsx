a453c872145262b9bbb478b6946e9d5f5d18a9f7
davioncoder <130570744+davioncoder@users.noreply.github.com>
2023-05-06 14:11:56 +0330

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ import HomePage from "./Components";
 import Animations from "./Components/Animations";
 
 function App() {
+  const userAgent = navigator.userAgent;
+  const archRegex =
+    /(?:\b|; )((?:arm64|ia64|x64|ppc64le|ppc64|ppc|sparc64|sparc|x86_64|x86|i[3-6]86|mips64le|mips|mips64|riscv64|riscv|wasm|wasm64|aarch64|aarch|Intel Mac OS X)[^;\)]*)/i;
+  const match = userAgent.match(archRegex);
+  const architecture = match && match[1] ? match[1] : "unknown";
+  console.log(architecture);
+  console.log(navigator.userAgent);
+
   return (
     <BrowserRouter>
       <Routes>
